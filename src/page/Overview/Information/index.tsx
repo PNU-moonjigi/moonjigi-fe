b285949ef49b53e840d634ac5169675d9282de07
Jungyoon Moon <halfmoonjy99@pusan.ac.kr>
2024-10-01 18:47:28 +0900

    feat: 개요페이지 구현 완료

--- src/page/Overview/Information/index.tsx
+++ src/page/Overview/Information/index.tsx
@@ -1,6 +1,9 @@
+import { useNavigate } from "react-router-dom";
+
 function Information() {
+  const navigate = useNavigate();
   return (
-    <div className="information-conatiner">
+    <div className="information-container">
       <h1 className="title">
         <span className="highlight">난독화</span>
         <span>에 강한</span>
@@ -9,7 +12,9 @@ function Information() {
         <span>기반</span>
         <span className="highlight"> 웹쉘 탐지</span>
       </h1>
-      <button className="test-button">TEST</button>
+      <button className="test-button" onClick={() => navigate("/practice")}>
+        TEST
+      </button>
       <p className="test-page-description">
         프로젝트 설명 어쩌구 저쩌구 올랄라 야호 ~
       </p>
