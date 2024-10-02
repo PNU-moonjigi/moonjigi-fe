165a8679cf53597c97dc069bcb0042b1d9fb18ae
harugi7 <gimme2721@gmail.com>
2024-10-02 20:13:22 +0900

    feat: MachineLearning 부분

---
+++ src/page/Process/MachineLearning.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const ML: React.FC = () => {
+    return (
+        <div className="container">
+            <div className="main-section">
+                    <h2 className="highlight">Machine Learning</h2>
+                    <ul className="list-item">
+                        <li className="list">
+                            TextRank
+                            <p className="description">
+                                난독화된 php 파일을 비난독화어쩌구저쩌구 추가설명
+                            </p>
+                        </li>
+                        <li className="list">
+                            TextRank
+                            <p className="description">
+                                난독화된 php 파일을 비난독화어쩌구저쩌구 추가설명
+                            </p>
+                        </li>
+                        <li className="list">
+                            TextRank
+                            <p className="description">
+                                난독화된 php 파일을 비난독화어쩌구저쩌구 추가설명
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            )
+            }
+
+            export default ML;
